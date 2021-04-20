ce34b56b67939f289d4c373801c9c6850811465c
genesis-steve <steve.x@gen-game.com>
2021-04-20 15:09:21 +0800

    add spin settings panel (prototype, no input area)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { IMainConfig, MainConfig } from 'src/config/MainConfig';
 import { HTMLElementCreator, HTMLElementType } from 'src/utils/HTMLElementCreator';
 import { ReelController } from 'src/components/reel/ReelController';
 import { SpinController } from 'src/components/spin/SpinController';
+import { SpinSettingsPanel } from 'src/components/external/SpinSettingsPanel';
+import { SpinSettingsConfig } from 'src/components/external/SpinSettingsConfig';
 
 window.onload = () => {
 	new GmaeApplication();
@@ -18,6 +20,7 @@ export class GmaeApplication {
 	protected mainContainer: HTMLDivElement;
 	protected reelContainer: ReelController;
 	protected spinContainer: SpinController;
+	protected spinSettingsPanel: HTMLDivElement;
 
 	protected pixi: PIXI.Application;
 
@@ -26,10 +29,10 @@ export class GmaeApplication {
 		document.title = this.appConfig.title;
 		document.body.style.overflow = 'hidden';
 		this.createElements();
-		this.tickStart();
+		this.startTick();
 	}
 
-	protected tickStart (): void {
+	protected startTick (): void {
 		function animate () {
 			requestAnimationFrame( animate )
 			TWEEN.update()
@@ -41,6 +44,7 @@ export class GmaeApplication {
 		this.mainContainer = <HTMLDivElement> document.getElementById( 'mainContainer' );
 		this.mainContainer.appendChild( HTMLElementCreator.createHTMLElement( HTMLElementType.BR ) );
 		this.setupPixiApplication();
+		this.setupExternalPanel();
 	}
 
 	protected setupPixiApplication (): void {
@@ -58,6 +62,15 @@ export class GmaeApplication {
 	protected createSpinPanel (): void {
 		this.spinContainer = new SpinController();
 		this.pixi.stage.addChild( this.spinContainer.mainContainer );
+	}
+
+	protected setupExternalPanel (): void {
+		this.createSpinSettingsPanel();
+	}
+
+	protected createSpinSettingsPanel (): void {
+		this.spinSettingsPanel = SpinSettingsPanel.init( new SpinSettingsConfig() );
+		this.mainContainer.appendChild( this.spinSettingsPanel );
 	}
 
 	protected addListeners (): void {
